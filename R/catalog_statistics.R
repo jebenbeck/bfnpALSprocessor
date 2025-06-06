be55d8756863996c58366465a0eba984a6c3cc98
Jakob Ebenbeck <jakob@ebenbeck.de>
2025-06-07 00:11:04 +0200

    updated catalog_statistics description and added tests

--- R/catalog_statistics.R
+++ R/catalog_statistics.R
@@ -1,12 +1,15 @@
-# Calculate point density of lascatalog files
+#' Calculate point density of lascatalog files
 #'
 #' @description Takes all files in a lascatalog and returns a table with statistics on point density, covex area covered,
 #' extemt, etc. for every tile
 #'
 #' @param lascatalog Object of class `lascatalog`
 #' @param parallel logical of length 1. Should the computation be split over several cores? Defaults to FALSE.
-#' @param n_cores numeric of length 1. If `parallelize = TRUE`, on how many cores should the computations be run on?
+#' @param n_cores numeric of length 1. If `parall = TRUE`, on how many cores should the computations be run on?
 #' Defaults to the value registered in `options("cores")[[1]]`, or, if this is not available, to `parallel::detectCores())`.
+#'
+#' @return data frame
+#' @export
 #'
 #' @examples
 #' ctg <- readALSLAScatalog("/path/to/lazfiles")
@@ -14,24 +17,23 @@
 
 catalog_statistics <- function(lascatalog, parallel = F, n_cores = 2){
 
-  #' set catalog options:
-  opt_select(lascatalog) <- "xy" #' read only xy because they are the only attributes of interest
+  # set catalog options:
+  opt_select(lascatalog) <- "xy" # read only xy because they are the only attributes of interest
   opt_independent_files(lascatalog) <- TRUE
 
-  #' function to get point density of las data:
+  # function to get point density of las data:
   calc_statistics = function(chunk) {
-    #return(chunk)
     las <- readLAS(chunk)
     if (is.empty(las)) return(NULL)
-    #' calculate point density:
+    # calculate point density:
     density = density(las)
-    #' calculate area covered by convex hull (not bbox):
+    # calculate area covered by convex hull (not bbox):
     area = lidR::area(las)
-    #' extract tile name from file (for merging information to other data later):
+    # extract tile name from file (for merging information to other data later):
     tilename = tools::file_path_sans_ext(basename(chunk@files))
-    #' extract extent of full tile:
+    # extract extent of full tile:
     extent_tile <- chunk@bbox
-    #' make data frame with necessary information:
+    # make data frame with necessary information:
     df <- tibble(Tile.name = tilename,
                  Point.density = density,
                  Area.covered = area,
@@ -42,7 +44,7 @@ catalog_statistics <- function(lascatalog, parallel = F, n_cores = 2){
     return(df)
   }
 
-  #' plan parallel processing
+  # plan parallel processing
   if (parallel == TRUE) {
     plan(multisession, workers = n_cores)
     message(paste("Parallel processing will be used with", n_cores, "cores"))
@@ -50,8 +52,8 @@ catalog_statistics <- function(lascatalog, parallel = F, n_cores = 2){
     warning("No parallel processing in use", call. = F, immediate. = T)
   }
 
-  #' apply function to catalog:
+  # apply function to catalog:
   statistics <- catalog_apply(lascatalog, calc_statistics)
-  #' merge the results to a single data frame:
+  # merge the results to a single data frame:
   statistics_merged_df <- bind_rows(statistics)
 }
